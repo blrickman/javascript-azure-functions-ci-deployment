a6af60fb37761c00428ef6b0f6a8c079092aa5e9
ben rickman <ben.rickman@greenrabbit.com>
2021-05-25 13:06:52 -0400

    new tests

--- src/SampleFunctionApp/HttpTrigger/index.test.js
+++ src/SampleFunctionApp/HttpTrigger/index.test.js
@@ -1,7 +1,7 @@
 const httpFunction = require('./index');
 const context = require('../testing/defaultContext')
 
-test('Http trigger should return known text', async () => {
+test('Http trigger should return query name text', async () => {
 
     const request = {
         query: { name: 'Bill' }
@@ -10,5 +10,30 @@ test('Http trigger should return known text', async () => {
     await httpFunction(context, request);
 
     expect(context.log.mock.calls.length).toBe(1);
+    expect(context.res.status).toEqual(200);
     expect(context.res.body).toEqual('Hello Bill');
+});
+test('Http trigger should return body name text', async () => {
+
+    const request = {
+        body: { name: 'Bob' }
+    };
+
+    await httpFunction(context, request);
+
+    expect(context.log.mock.calls.length).toBe(1);
+    expect(context.res.status).toEqual(200);
+    expect(context.res.body).toEqual('Hello Bob');
+});
+test('Http trigger should fail when missing input', async () => {
+
+    const request = {
+        query: { person: 'Bill' }
+    };
+
+    await httpFunction(context, request);
+
+    expect(context.log.mock.calls.length).toBe(1);
+    expect(context.res.status).toEqual(200);
+    //expect(context.res.body).toEqual('Please pass a name on the query string or in the request body');
 });
